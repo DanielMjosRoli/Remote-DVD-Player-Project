d6ba9724291c3c00599fbae70d4370c02be2f183
Daniel.Roli <daniel.mrfush@gmail.com>
2026-02-23 14:29:19 +0100

    Added the Domain models for the DB

--- Backend/API/Infrastructure/MediaDBContext.cs
+++ Backend/API/Infrastructure/MediaDBContext.cs
@@ -8,4 +8,18 @@ public class MediaDBContext : DbContext
     }
 
     public DbSet<User> Users => Set<User>();
+    public DbSet<Movie> Movies => Set<Movie>();
+    public DbSet<MediaFile> MediaFiles => Set<MediaFile>();
+    public DbSet<StorageVolume> StorageVolumes => Set<StorageVolume>();
+    public DbSet<Genre> Genres => Set<Genre>();
+    public DbSet<MovieGenre> MovieGenres => Set<MovieGenre>();
+    public DbSet<Collection> Collections => Set<Collection>();
+    public DbSet<CollectionMovie> CollectionMovies => Set<CollectionMovie>();
+    public DbSet<WatchHistory> WatchHistories => Set<WatchHistory>();
+    public DbSet<Rating> Ratings => Set<Rating>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(MediaDBContext).Assembly);
+    }
 }
